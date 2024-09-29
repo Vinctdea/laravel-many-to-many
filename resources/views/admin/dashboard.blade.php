36b876c5bea94d8a782454898982a8ee218eaada
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-29 23:31:18 +0200

    fix: style dashboard index

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,15 +9,17 @@
             <div class="col-12 d-flex justify-content-center ">
                 <h3>Ultimo lavoro</h3>
             </div>
-            <div class="col-12 ">
-                <table class="table">
+            <div class="col-9 m-auto p-3">
+                <table class="table table-dark table-hover">
                     <thead>
                         <tr>
                             <th scope="col">ID</th>
                             <th scope="col">Nome</th>
                             <th scope="col">Tempo di realizzazione</th>
                             <th scope="col">Categoria</th>
+                            <th scope="col">ultima modifica</th>
                             <th scope="col">Azioni</th>
+
                         </tr>
                     </thead>
                     <tbody>
@@ -26,8 +28,9 @@
                             <td>{{ $jobs->title }}</td>
                             <td>{{ $jobs->processing_time }} Settimane</td>
                             <td>{{ $jobs->content }}</td>
+                            <td>{{ $jobs->created_at->format('d/m/Y') }}</td>
                             <td>
-                                <a class="btn btn-dark" href="{{ route('admin.jobs.show', $jobs->id) }}">Vai</a>
+                                <a class="btn btn-primary" href="{{ route('admin.jobs.show', $jobs->id) }}">Vai</a>
                             </td>
 
                         </tr>
